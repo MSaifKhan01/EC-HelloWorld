f71d512e647fd4ee4f3e23577054dd2260195eaf
Mohd Saif Khan <msaifkhan5038@gmail.com>
2024-03-07 21:16:36 +0530

    Update helloworld.go

--- helloworld.go
+++ helloworld.go
@@ -11,7 +11,7 @@ func handler(w http.ResponseWriter, r *http.Request) {
 	log.Print("helloworld: received a request")
 	target := os.Getenv("TARGET")
 	if target == "" {
-		target = "Knative--02"
+		target = "Knative--03"
 	}
 	fmt.Fprintf(w, "Hello for Test my API ---02 success  %s!\n", target)
 }
